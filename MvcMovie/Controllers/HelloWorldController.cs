937adae9d9eca1992ec67ec4a418b2b62d081f92
dimchen <timlaugh@gmail.com>
2014-10-31 15:24:03 -0700

    ver1

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        //public string Index()
+        //{
+        //    return "This is my Action";
+        //    //reut
+        //}
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            //return HttpUtility.HtmlEncode("hello " + name + " "+ numTimes );
+            ViewBag.Message = "Hello" + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+
+        public string Action1()
+        {
+            return "Our Welcome message";
+        }
+
+        public string action2()
+        {
+            return "Action 2 message";
+        }
+	}
+}
